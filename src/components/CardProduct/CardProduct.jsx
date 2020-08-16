096828346daff6a690a96ddeed6b3bda51a042ce
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-08-16 11:56:59 +0300

    add: Picker

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -5,12 +5,16 @@ import PropTypes from 'prop-types'
 import BgImage from 'components/BgImage/BgImage'
 import css from './cardProduct.module.scss'
 import useDevice from 'hooks/useDevice'
+import Picker from 'components/Picker/Picker'
 
 const CardProduct = ({ className, img, title, price }) => {
   const { currentDevice } = useDevice()
   return (
     <div className={cn(css[currentDevice], css.container)}>
       <BgImage img={img} className={css.img} />
+      <div className={css.picker}>
+        <Picker />
+      </div>
       <div className={css.wrapper}>
         <h3 className={css.title}>{title}</h3>
         {price && <div className={css.price}>{price} руб.</div>}
---
+++ src/components/Picker/Picker.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import cn from 'classnames'
+import PropTypes from 'prop-types'
+import css from 'components/Picker/Picker.module.scss'
+import useDevice from 'hooks/useDevice'
+
+const Picker = ({ className, count = 0, onChange, variant }) => {
+  const { currentDevice } = useDevice()
+  const addValue = () => onChange(count + 1)
+
+  return (
+    <div className={cn(css[variant], css[currentDevice], className)} onClick={e => e.preventDefault()}>
+      {
+        !count
+          ? (
+            <div className={css.plus} onClick={addValue}>+</div>
+          )
+          : (
+            <div className={css.picker}>
+              <div className={css.signMinus} onClick={() => onChange(count - 1)}>-</div>
+              <div>{count}</div>
+              <div className={css.signPlus} onClick={addValue}>+</div>
+            </div>
+          )
+      }
+    </div>
+  )
+}
+Picker.propTypes = {
+  className: PropTypes.string,
+  count: PropTypes.number,
+  onChange: PropTypes.func,
+  variant: PropTypes.oneOf(['black'])
+}
+export default React.memo(Picker)
